2c35519a5493b40192f5dd3cc4ca9727eb959bb8
Steve Taylor <staylor110@gmail.com>
2015-01-07 01:13:44 -0600

    Update 1/7/2015, stj

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
